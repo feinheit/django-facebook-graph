a176c8c342433e6a6ae69e8717b4b59183e65b54
Simon Schmid <ssc@feinheit.ch>
2010-07-21 20:48:25 +0200

    merged

--- backends/registration/views.py
+++ backends/registration/views.py
@@ -55,3 +55,56 @@ def logout(request, next_page=None, template_name='registration/logged_out.html'
     if cookie:
         response.delete_cookie("fbs_" + settings.FACEBOOK_APP_ID)
     return response
+
+def connect(request, redirect_field_name=REDIRECT_FIELD_NAME):
+    cookie = facebook.get_user_from_cookie(request.COOKIES, 
+                                           settings.FACEBOOK_APP_ID, 
+                                           settings.FACEBOOK_APP_SECRET)
+    redirect_to = request.REQUEST.get(redirect_field_name, reverse('account'))
+    
+    if request.user.is_authenticated() and cookie:
+        try:
+            graph = facebook.GraphAPI(cookie["access_token"])
+            profile = graph.get_object("me")
+        except facebook.GraphAPIError, e:
+            return render_to_response(
+                        'registration/facebook/graph_error.html', {'error': e},
+                        context_instance=RequestContext(request))
+        
+        # if the user has already a facebook connection, abort and show
+        # error message
+        if hasattr(request.user, 'facebookuser'):
+            connected_profile = graph.get_object("%s" % request.user.facebookuser.id)
+            ctx = {'fb_name' : connected_profile['name'],
+                   'fb_link' : connected_profile['link'],
+                   'username' : request.user.username}
+            return render_to_response(
+                        'registration/facebook/already_connected.html', ctx,
+                        context_instance=RequestContext(request))
+        try:
+            # if that facebook user already exists, abort and show error message
+            fb_user = FacebookUser.objects.get(id=cookie['uid'])
+            ctx = {'fb_name' : profile["name"],
+                   'user' : fb_user.user}
+            return render_to_response(
+                        'registration/facebook/user_exists.html', ctx,
+                        context_instance=RequestContext(request))
+        except FacebookUser.DoesNotExist:
+            fb_user = FacebookUser(id=cookie['uid'], 
+                                   user=request.user,
+                                   profile_url=profile["link"],
+                                   access_token=cookie["access_token"])
+            fb_user.save()
+            return HttpResponseRedirect(redirect_to)
+    
+    elif request.user.is_authenticated():
+        ctx = {'username' : request.user.username}
+        # if no cookie is present, the user did not authorize the application
+        # in facebook. show the facebook connect button
+        return render_to_response('registration/facebook/connect.html', ctx,
+                                  context_instance=RequestContext(request))
+    else:
+        # there is no facebook graph cookie and the user is not logged in
+        # -> redirect to login page
+        return HttpResponseRedirect(reverse('auth_login'))
+    
