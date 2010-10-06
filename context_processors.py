cc54e55f0ca8edf4506b2daadf63c52766da3bc8
Simon Schmid <ssc@feinheit.ch>
2010-10-06 12:54:02 +0200

    settings dont have to be configured

--- context_processors.py
+++ context_processors.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
 def facebook_keys(request):
-    return { 'FACEBOOK_API_KEY' : settings.FACEBOOK_API_KEY,
-             'FACEBOOK_APP_SECRET' : settings.FACEBOOK_APP_SECRET, 
-             'FACEBOOK_APP_ID' : settings.FACEBOOK_APP_ID }
+    return { 'FACEBOOK_API_KEY' : getattr(settings, 'FACEBOOK_API_KEY', ''),
+             'FACEBOOK_APP_SECRET' : getattr(settings, 'FACEBOOK_APP_SECRET', ''), 
+             'FACEBOOK_APP_ID' : getattr(settings, 'FACEBOOK_APP_ID', '') }
