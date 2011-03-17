eee086a932efabb0d8035344f5508d9ff4e4cb5f
Simon Schmid <ssc@feinheit.ch>
2011-03-17 14:06:48 +0100

    merge confilicts resolved

--- models.py
+++ models.py
@@ -1,13 +1,25 @@
 import logging
 logger = logging.getLogger(__name__)
 
+from django import forms
 from django.contrib.auth.models import User
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
+from django.utils import simplejson as json
+from django.utils.translation import ugettext_lazy as _
+
 
 from fields import JSONField
 from utils import get_graph
 
 class Base(models.Model):
+    """ Last Lookup JSON """
+    _graph = models.TextField(blank=True, null=True)
+    
+    @property
+    def graph(self):
+        return json.loads(self._graph)
+    
     class Meta:
         abstract = True
         
@@ -16,7 +28,7 @@ class Base(models.Model):
         response = graph.request(str(self.id))
         
         if response:
-            self._graph = response
+            self._graph = json.dumps(response, cls=DjangoJSONEncoder)
             for prop, (val) in response.items():
                 if hasattr(self, '_%s' % prop):
                     setattr(self, '_%s' % prop, val)
@@ -24,14 +36,21 @@ class Base(models.Model):
             logger.debug('graph not retrieved', extra=response)
         
         if save: 
-            super(Base, self).save(*args, **kwargs)
+            self.save()
         return self
     
-    def save(self, refresh=True, *args, **kwargs):
+    def clean(self, refresh=True, request=None, access_token=None, \
+             client_secret=None, client_id=None, *args, **kwargs):
+        if self._graph:
+            try:
+                self._graph = json.dumps(json.loads(self._graph), cls=DjangoJSONEncoder)
+            except ValueError:
+                raise forms.ValidationError(_('Invalid JSON Data'))
+        
         if refresh:
-            self.refresh(*args, **kwargs)
-        else:
-            super(Base, self).save(*args, **kwargs)
+            self.refresh(request=request, access_token=access_token, \
+                        client_secret=client_secret, client_id=client_id, \
+                        *args, **kwargs)
 
 class FacebookUser(Base):
     id = models.BigIntegerField(primary_key=True, unique=True)
