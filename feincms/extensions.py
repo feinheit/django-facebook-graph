457136ad170830ece42d5c2766b95abb23cb9254
Simon Schmid <ssc@feinheit.ch>
2011-03-30 18:48:20 +0200

    extensions should be null

--- feincms/extensions.py
+++ feincms/extensions.py
@@ -5,7 +5,7 @@ from facebook.models import Application
 
 
 def facebook_application(cls, admin_cls):
-    cls.add_to_class('facebook_application', models.ForeignKey(Application, help_text=('Link this page to a facebook app. Used for Facebook Tabs, to determine the underlaying FB App')))
+    cls.add_to_class('facebook_application', models.ForeignKey(Application, blank=True, null=True, help_text=('Link this page to a facebook app. Used for Facebook Tabs, to determine the underlaying FB App')))
     
     admin_cls.fieldsets.append((_('Facebook Application'),{
         'fields' : ('facebook_application',),
