fb9ff4f51418ac1db537a53157c7744730327f4b
Simon Schmid <ssc@feinheit.ch>
2011-04-28 15:46:56 +0200

    get rid of original facebook submodule

--- facebook/__init__.py
+++ facebook/__init__.py
@@ -1,3 +1,1 @@
-import os
-FB_BASEDIR = os.path.dirname(os.path.abspath(__file__))
-execfile(os.path.join(FB_BASEDIR, 'python-sdk', 'src', 'facebook.py'))
+from api import GraphAPI, GraphAPIError, get_user_from_cookie
