ba824329419f5b90e54177d6292c4028229c0c44
Matthias Kestenholz <mk@spinlock.ch>
2011-06-16 19:16:32 +0200

    Fix type error when creating a new user while authenticating through FB Connect

--- facebook/backends/authentication/__init__.py
+++ facebook/backends/authentication/__init__.py
@@ -8,35 +8,36 @@ from django.template.defaultfilters import slugify
 import facebook
 from facebook.models import User as FacebookUser
 
-class AuthenticationBackend:
+class AuthenticationBackend(object):
+    supports_anonymous_user = False
+
     def authenticate(self, uid=None, access_token=None):
         try:
             graph = facebook.GraphAPI(access_token)
             profile = graph.get_object("me")
         except facebook.GraphAPIError:
             return None
-        
+
         try:
             facebook_user = FacebookUser.objects.get(id=uid)
             facebook_user.access_token = access_token
             facebook_user.save()
             user = facebook_user.user
         except ObjectDoesNotExist:
-            facebook_user = FacebookUser(id=uid, 
-                                         profile_url=profile["link"], 
+            facebook_user = FacebookUser(id=uid,
                                          access_token=access_token)
             user, c = User.objects.get_or_create(
                                 username=slugify(profile["name"]),
-                                email=profile["email"], 
+                                email=profile["email"],
                                 password=hashlib.md5(uid).hexdigest())
             user.first_name = profile['first_name']
             user.last_name = profile['last_name']
             user.save()
             facebook_user.user = user
             facebook_user.save()
-        
+
         return user
-    
+
     def get_user(self, user_id):
         try:
             return User.objects.get(id=user_id)
