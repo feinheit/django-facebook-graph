83a937b2d142f43d9ab8a943f6cba8c736241abe
Matthias Kestenholz <mk@spinlock.ch>
2011-06-16 19:19:07 +0200

    Fix typo in HTML comment

--- facebook/feincms/views.py
+++ facebook/feincms/views.py
@@ -6,12 +6,12 @@ from feincms.module.page.models import Page
 
 def redirect_to_slug(request):
     """ tries to redirect the user with help of the facebook signed_request page params (admin, liked) """
-    
+
     try:
         facebook_page = request.session['facebook']['signed_request']['page']
     except e:
         return '<!-- could not redirect to slug via facebook page signed request params: %s -->' % e
-    
+
     page = Page.objects.from_request(request, best_match=True)
     if facebook_page['admin'] and facebook_page['liked']:
         try:
@@ -28,8 +28,8 @@ def redirect_to_slug(request):
             return redirect(page.get_children().filter(slug='admin')[0])
         except IndexError:
             pass
-    
+
     try:
         return redirect(page.get_children().filter(slug='unliked')[0])
     except IndexError:
-        return '<!-- no chlidpage with matching slug found. looked for slugs: admin-liked, liked, admin, unliked -->'
+        return '<!-- no childpage with matching slug found. looked for slugs: admin-liked, liked, admin, unliked -->'
