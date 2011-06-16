4f66675fede88b02ea963422bd144e0f43c49f5a
Matthias Kestenholz <mk@spinlock.ch>
2011-06-16 19:20:00 +0200

    FB content_type_extension: It's recommended to extend the concrete content type

--- facebook/feincms/extensions.py
+++ facebook/feincms/extensions.py
@@ -19,7 +19,7 @@ def content_type_extension(cls, admin_cls=None):
     Sample implementation (models.py):
 
     from facebook.feincms.extensions import content_type_extension
-    content_type_extension(RichTextContent)
+    content_type_extension(Page.create_content_type(RichTextContent))
     """
     RENDER_CHOICES = (
         (0, _('both')),
@@ -29,37 +29,37 @@ def content_type_extension(cls, admin_cls=None):
 
     # save the original render method
     cls.original_render = cls.render
-    
+
     cls.add_to_class('render_like', models.PositiveSmallIntegerField(_('render like'), blank=True, null=True, default=0, choices=RENDER_CHOICES, help_text=_('Render this content only, if requesting user likes the page.')))
     cls.add_to_class('app_installed', models.PositiveSmallIntegerField(_('app installed'), blank=True, null=True, default=0, choices=RENDER_CHOICES, help_text=_('Render this content only, if requesting user has installed the corresponding app.')))
 
     def render(instance, request, context, **kwargs):
         """ override render function. renders on dependency of the render settings. """
-        
+
         try:
             # will crash, if there is no facebook and/or signed request in the session
             user_liked = request.session['facebook']['signed_request']['page']['liked']
         except:
             return instance.original_render(**kwargs)
-        
+
         try:
             # if the user has not installed the app, there will be no user_id
             user_id = request.session['facebook']['signed_request']['user_id']
             app_installed = True
         except:
             app_installed = False
-        
+
         render_because_of_like = True
         render_because_of_app_installed = True
-        
+
         if (instance.render_like == 2 and user_liked) or \
             (instance.render_like == 1 and not user_liked):
             render_because_of_like = False
-        
+
         if (instance.app_installed == 2 and app_installed) or \
             (instance.app_installed == 1 and not app_installed):
             render_because_of_app_installed = False
-        
+
         if render_because_of_like and render_because_of_app_installed:
             return instance.original_render(**kwargs)
         else:
