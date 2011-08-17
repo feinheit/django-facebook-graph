13108e5dba38668328640082d4119bef16ae3c19
Matthias Kestenholz <mk@spinlock.ch>
2011-08-17 12:16:32 +0200

    Stop using deprecated page manager methods

--- facebook/feincms/utils.py
+++ facebook/feincms/utils.py
@@ -2,24 +2,24 @@ from feincms.module.page.models import Page
 
 def get_application_from_request(request):
     try:
-        page = Page.objects.from_request(request, best_match=True)
+        page = Page.objects.for_request(request, best_match=True)
         return getattr(page, 'facebook_application', None)
     except Page.DoesNotExist:
         return None
 
 def get_page_from_request(request):
     try:
-        page = Page.objects.from_request(request, best_match=True)
+        page = Page.objects.for_request(request, best_match=True)
         return getattr(page, 'facebook_page', None)
     except Page.DoesNotExist:
         return None
 
 def get_tab_url_from_request(request):
     try:
-        page = Page.objects.from_request(request, best_match=True)
+        page = Page.objects.for_request(request, best_match=True)
     except Page.DoesNotExist:
         return None
-    
+
     fb_app = getattr(page, 'facebook_application', None)
     fb_page = getattr(page, 'facebook_page', None)
 
