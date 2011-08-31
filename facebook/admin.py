82be1f34551fd7aba1e8bd843eebb5efeff8727c
Simon Bächler <sb@feinheit.ch>
2011-08-31 09:57:24 +0200

    added score admin

--- facebook/admin.py
+++ facebook/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
-from models import User, Photo, Page, Event, Request, TestUser, Post
+from models import User, Photo, Page, Event, Request, TestUser, Post, Score
 from utils import get_graph
 
 
@@ -117,4 +117,21 @@ class PostAdmin(AdminBase):
     
     
 admin.site.register(Post, PostAdmin)
+
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('user', 'score')
+    readonly_fields = ('user', 'score')
+    search_fields = ('user',)
+    ordering = ['score']
     
+""" Because Facebook does not yet correctly support score the score model is just experimental.
+    Import it in your project admin file:
+    
+    from facebook.models import Score
+    from facebook.admin import ScoreAdmin
+    admin.site.register(Score, ScoreAdmin)
+"""
+
+
+
+
