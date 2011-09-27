a0b54b0086e3381b500bbd10801e7e3647b67f32
Simon Bächler <sb@feinheit.ch>
2011-09-27 15:38:54 +0200

    moved rarely used js function to another script. Fix in get_perms.

--- facebook/static/facebook/fb_utils.js
+++ facebook/static/facebook/fb_utils.js
@@ -13,25 +13,26 @@ function canvas_resize() {
 FQ = {
     queue: new Array(),
     add: function(f) { if (typeof f == 'function') { this.queue.push(f); } },
-    run: function() { while(this.queue.length > 0) { f = this.queue.pop(); f();
-                      canvas_resize(); } }
+    run: function() { while(this.queue.length > 0) { f = this.queue.pop(); f(); }
+                      canvas_resize();
+                     }
 };
 var fb = {
     user: {}, // DEPRECATED Property. Here for backwards compatibility.
     get_perms : function(callback) {
-                    var _perms = false;
+                    fb._perms = fb._perms || false;
                     return (function() {
-                        if (_perms) {
-                            callback(_perms);
-                            return true;
-                        }
-                        _perms = [];
+                        if (fb._perms) {
+                            if(callback) {callback(fb._perms)};
+                            return fb._perms;
+                        } 
+                        fb._perms = [];
                         FB.api('/me/permissions/', function(data){
-                            for (var i in data['data'][0]) {_perms.push(i);}
-                            callback(_perms);
+                            for (var i in data['data'][0]) {fb._perms.push(i);}
+                            if(callback) {callback(fb._perms);}
                             return true;
                         });
-                    })()
+                    })();
                 }
     };
 
@@ -65,44 +66,4 @@ FQ.add(function(){
         }
     });
 });
-
-function log_error(url, message, csrf_token) {
-    $.post( url,
-        {'csrfmiddlewaretoken': csrf_token,
-         'message': message },
-         function(response){
-             log(response);
-         }, 'text'
-    );
-}
-function post_to_url(path, params, method) {
-    method = method || "post"; // Set method to post by default, if not specified.
-
-    // The rest of this code assumes you are not using a library.
-    // It can be made less wordy if you use one.
-    var form = document.createElement("form");
-    form.setAttribute("method", method);
-    form.setAttribute("action", path);
-
-    for(var key in params) {
-        var hiddenField = document.createElement("input");
-        hiddenField.setAttribute("type", "hidden");
-        hiddenField.setAttribute("name", key);
-        hiddenField.setAttribute("value", params[key]);
-
-        form.appendChild(hiddenField);
-    }
-
-    document.body.appendChild(form);
-    form.submit();
-}
-
-// Use this function for facebook connect if the user is connected to facebook
-// but not to Django. This will send the signed request. After this, redirect to auth/connect.
-
-function force_signed_request(fb) {
-    if (fb.status != 'connected') { return false; }
-    params = { 'signed_request' : fb.authResponse.signedRequest }
-    post_to_url(window.location.toString(), params);
-}
 
