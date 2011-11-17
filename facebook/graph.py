d302c5333a3fc60d93b3d8f145e6417b4d7f7aee
Simon Schmid <ssc@feinheit.ch>
2011-11-17 15:18:46 +0100

    fix bug: the graph response is a dict, not an object.

--- facebook/graph.py
+++ facebook/graph.py
@@ -408,7 +408,7 @@ class Graph(GraphAPI):
             return self._user_id
         else:
             me = self._get_me(self.access_token)
-            return getattr(me, 'id', None)
+            return me.get('id', None)
 
     @property
     def user_id(self):
@@ -416,7 +416,7 @@ class Graph(GraphAPI):
             return int(self._user_id)
         else:
             me = self._get_me(self.access_token)
-            id = getattr(me, 'id', None)
+            id = me.get('id', None)
             return int(id) if id else None
         
     def type(self, token=None):
