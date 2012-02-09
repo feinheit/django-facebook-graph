0e6a1de91ed90cbfa35223e78c2639d95a352f4b
Simon Schmid <ssc@feinheit.ch>
2012-02-09 11:56:17 +0100

    make _username as readonly field

--- facebook/modules/profile/user/admin.py
+++ facebook/modules/profile/user/admin.py
@@ -6,7 +6,7 @@ from .models import User
 
 class UserAdmin(AdminBase):
     list_display = ('id', 'profile_link', '_email', 'access_token', 'user', '_name', 'created', 'updated',)
-    readonly_fields = ('friends', '_name', '_first_name', '_last_name', '_link', '_birthday', '_email', '_location', '_gender', '_graph')
+    readonly_fields = ('friends', '_name', '_locale', '_username', '_first_name', '_last_name', '_link', '_birthday', '_email', '_location', '_gender', '_graph')
     search_fields = ('id', '_name')
 
 
