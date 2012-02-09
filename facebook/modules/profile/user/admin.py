ce4a9964ec66deaeefc6f7a42cf1a4e4b44775c4
Simon Schmid <ssc@feinheit.ch>
2012-02-09 11:14:42 +0100

    blub

--- facebook/modules/profile/user/admin.py
+++ facebook/modules/profile/user/admin.py
@@ -5,7 +5,7 @@ from facebook.modules.base import AdminBase
 from .models import User
 
 class UserAdmin(AdminBase):
-    list_display = ('id', 'profile_link', 'email', 'access_token', 'user', '_name', 'created', 'updated',)
+    list_display = ('id', 'profile_link', '_email', 'access_token', 'user', '_name', 'created', 'updated',)
     readonly_fields = ('friends', '_name', '_first_name', '_last_name', '_link', '_birthday', '_email', '_location', '_gender', '_graph')
     search_fields = ('id', '_name')
 
