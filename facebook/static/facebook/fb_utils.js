19f5cd1833d8da328cf3a8bf62753a2f8bb77239
Simon Bächler <sb@feinheit.ch>
2012-02-28 13:44:54 +0100

    remove IE hack due to fix in JS SDK

--- facebook/static/facebook/fb_utils.js
+++ facebook/static/facebook/fb_utils.js
@@ -60,9 +60,10 @@ window.fbAsyncInit = function() {
              xfbml: true, oauth: true,
              channelUrl : document.location.protocol + '//' + document.location.host + FACEBOOK_CHANNEL_URL }
     );
+
     canvas_resize();
-    // This is needed so IE doesn't throw a permission denied exception.
-    FB.UIServer.setLoadedNode = function (a, b) { FB.UIServer._loadedNodes[a.id] = b; };
+    // Apparently this bug has been fixed.
+    // FB.UIServer.setLoadedNode = function (a, b) { FB.UIServer._loadedNodes[a.id] = b; };
     FB.getLoginStatus(function(response) {
       log(response);
       fb['status'] = response.status;
