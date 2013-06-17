0d68852a051bf23c1dcc681dc8694d34ce7f8fe5
Fabian Germann <fg@feinheit.ch>
2013-06-17 16:17:57 +0200

    Remove verify_exists for Django 1.5 compatibility

--- facebook/modules/connections/likes/models.py
+++ facebook/modules/connections/likes/models.py
@@ -11,7 +11,7 @@ from django.contrib.contenttypes import generic
 from facebook.modules.profile.user.models import User
 from facebook.modules.profile.page.models import Page
 
-        
+
 class Like(models.Model):
     """ The users likes. Uses a generic foreign key to Page so it doesn't need
         a Page instance but you still get all the ORM goodness.
@@ -27,46 +27,46 @@ class Like(models.Model):
     # This field exists in the facebook table. But does not seem to be serverd by the graph api.
     post_id = models.CharField(_('Post id'), max_length=255, blank=True, null=True)
     _created_time = models.DateTimeField(blank=True, null=True)
-    
+
     class Meta:
         verbose_name = _('Profile Like')
         verbose_name_plural = _('Profile Likes')
         app_label = 'facebook'
-    
+
     def __unicode__(self):
         return u'%s likes %s' % (self.user, self._name)
-    
+
     @property
     def _id(self):
         return self.content_id
-    
+
     @_id.setter
     def _id(self, value):
         self.content_type = ContentType.objects.get_for_model(Page)
         self.content_id = value
-    
+
     @property
     def created_time(self):
         return self._created_time
-    
+
     @created_time.setter
     def created_time(self, val):
         if '+' in val: # ignore timezone for now ...
                         val = val[:-5]
         self._created_time = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S")
 
-    
+
 class URLLike(models.Model):
     """ This model can be used when querying the url_like table. """
     user = models.ForeignKey(User)
-    url = models.URLField(_('URL'), max_length=500, verify_exists=False, blank=True, null=True)
-    
+    url = models.URLField(_('URL'), max_length=500, blank=True, null=True)
+
     class Meta:
         verbose_name = _('URL Like')
         verbose_name_plural = _('URL Likes')
         app_label = 'facebook'
-        
+
     def __unicode__(self):
         return unicode(self.url)
-    
+
 
